afd0468d8cd200bca2ff650e6c1d67d5edf46e50
ahmed-muhamad <ahmed.elzowawi@outlook.com>
2023-02-28 23:58:40 +0100

    Add leader key and one config to nvim tree

--- nvim/lua/ahmed/core/remaps.lua
+++ nvim/lua/ahmed/core/remaps.lua
@@ -1,5 +1,9 @@
 local keymap = vim.keymap
 
+vim.g.mapleader = ' '
+vim.opt.langmap = "ne;jk"
+
 keymap.set("n", "<S-n>", "n")
 keymap.set("n", "<S-j>", "N")
 keymap.set("n", "<S-e>", "e")
+keymap.set("n", "<leader>k", vim.cmd.NvimTreeToggle)
