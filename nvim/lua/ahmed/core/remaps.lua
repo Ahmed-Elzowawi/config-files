be97bcb86470410c730e7c42c790096d95cb9959
ahmed-muhamad <ahmed.elzowawi@outlook.com>
2023-03-10 23:55:34 +0100

    Add new remaps and change nvim-tree remap from k to t

--- nvim/lua/ahmed/core/remaps.lua
+++ nvim/lua/ahmed/core/remaps.lua
@@ -6,4 +6,10 @@ vim.opt.langmap = "ne;jk"
 keymap.set("n", "<S-n>", "n")
 keymap.set("n", "<S-j>", "N")
 keymap.set("n", "<S-e>", "e")
-keymap.set("n", "<leader>k", vim.cmd.NvimTreeToggle)
+
+keymap.set("n", "<leader>t", vim.cmd.NvimTreeToggle)
+
+keymap.set("n", "<leader>k", "<C-w>k")
+keymap.set("n", "<leader>j", "<C-w>j")
+keymap.set("n", "<leader>l", "<C-w>l")
+keymap.set("n", "<leader>h", "<C-w>h")
