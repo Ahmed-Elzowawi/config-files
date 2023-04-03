7ba62f73a4957518c2c40c3a606c78387ec16ce5
ahmed-muhamad <ahmed.elzowawi@outlook.com>
2023-04-03 23:39:37 +0100

    Change telescope theme, as well as add and change a few keybinds

--- nvim/lua/ahmed/after/telescope.lua
+++ nvim/lua/ahmed/after/telescope.lua
@@ -1,6 +1,29 @@
+local actions = require "telescope.actions"
+require("telescope").setup {
+	defaults = {
+		mappings = {
+			i = {
+				["<C-n>"] = actions.move_selection_next,
+				["<C-e>"] = actions.move_selection_previous,
+			}
+		}
+	},
+	pickers = {
+		find_files = {
+			theme = "dropdown",
+		},
+		buffers = {
+			mappings = {
+				i = {
+					["<C-d>"] = actions.delete_buffer + actions.move_to_top,
+				},
+			}
+		}
+	}
+}
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
